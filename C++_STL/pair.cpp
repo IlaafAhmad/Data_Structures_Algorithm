1b0b7567af6b1210ca3a01dcc75f98b7eee9077d
Ilaaf Ahmad <ilaafahmad1478@gmail.com>
2024-09-18 16:16:08 +0530

    18092024

---
+++ C++_STL/pair.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    pair<int, int> pr1, pr2;
+    cin >> pr1.first >> pr1.second;
+    cin >> pr2.first >> pr2.second;
+    // pr.first = 2;
+    // pr.second = 5;
+
+    cout << pr1.first << " " << pr2.first;
+
+    return 0;
+
+}
