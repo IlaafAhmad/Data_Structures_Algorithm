5cbfd8a89d3164b820861d437765166dfdecc832
IlaafAhmad <149512960+IlaafAhmad@users.noreply.github.com>
2024-09-25 22:52:29 +0530

    Create reverse_array.cpp

---
+++ TUF+_DSA/Beginner Problems/Basic Arrays/reverse_array.cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:
+    void reverse(int arr[], int n){
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right) {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+};
